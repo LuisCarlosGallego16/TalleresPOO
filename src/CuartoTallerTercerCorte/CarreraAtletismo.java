38132071f1d42b1b772d2a5b4aa2ac20548bdfb1
Luis Carlos Gallego <luiscarlosgallegomorales94@gmail.com>
2024-12-03 21:11:32 -0500

    Creacion de main y objetos atleta para hacer funcional el metodo para empezar la carrera

--- src/CuartoTallerTercerCorte/CarreraAtletismo.java
+++ src/CuartoTallerTercerCorte/CarreraAtletismo.java
@@ -9,6 +9,30 @@ package CuartoTallerTercerCorte;
  * @author luisc
  */
 public class CarreraAtletismo {
+
     public static void main(String[] args) {
+        Atleta atleta1 = new Atleta("Pepe");
+        Atleta atleta2 = new Atleta("Juanito");
+        Atleta atleta3 = new Atleta("Toribio");
+        Atleta atleta4 = new Atleta("Luisito");
+        Atleta atleta5 = new Atleta("Prado");
+
+        atleta1.start();
+        atleta2.start();
+        atleta3.start();
+        atleta4.start();
+        atleta5.start();
+
+        try {
+            atleta1.join();
+            atleta2.join();
+            atleta3.join();
+            atleta4.join();
+            atleta5.join();
+        } catch (InterruptedException e) {
+            System.out.println("LA CARRERA FUE INTERRUMPIDA");
+        }
+        System.out.println("LA CARRERA TERMINO!");
+
     }
 }
